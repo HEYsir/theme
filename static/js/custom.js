7f2781b04266538e42bd0f92c2a27e8824e33c36
HEYsir <wafx-1314@163.com>
2022-03-06 19:55:09 +0800

    重新转服务端渲染
    原因很简单，当前返回的是整个页面信息，懒得做信息区分，不然还得调整请求

--- static/js/custom.js
+++ static/js/custom.js
@@ -20,7 +20,8 @@ function openDialog(url){
         success: function(result){
             // const markdown = "**Markdown**"
             // console.log("\nmarkdown input:", markdown, "\n")
-            let Md2HTMLCon = lute.MarkdownStr("", result)
+            // let Md2HTMLCon = lute.MarkdownStr("", result)
+            Md2HTMLCon = result
             $("#pop-article").html(Md2HTMLCon);
             document.getElementById('pop-article').style.display='block';
         },
@@ -32,8 +33,9 @@ function openDialog(url){
 }
 function closeDialog(){
     window.parent.location.reload();
-    document.getElementById('light').style.display='none';
-    document.getElementById('fade').style.display='none'
+    // document.getElementById('pop-article').style.display='none';
+    // document.getElementById('light').style.display='none';
+    // document.getElementById('fade').style.display='none'
 }
 
 $(document).bind('keydown', 'esc', function (evt) {
